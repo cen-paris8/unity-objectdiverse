a4ba29c1d4e814dd975de883e75c4c2b94786987
xofni <fakenewsProject@hotmail.com>
2019-09-16 11:02:29 +0200

    Add Test okou

---
+++ Assets/Scripts/Test/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Student : IComparable<Student>
+{
+    public string name;
+    public int value;
+
+    public Student(string newName, int newValue)
+    {
+        name = newName;
+        value = newValue;
+    }
+
+    public int CompareTo(Student other)
+    {
+        if (other == null)
+        {
+            return 1;
+        }
+        return other.value - value;
+    }
+}
---
+++ Assets/Scripts/Test/Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public List<Student> students = new List<Student>();
+
+    void Start()
+    {
+        Debug.Log("Start");
+        students.Add(new Student("Kevin", 12));
+        students.Add(new Student("Tracy", 15));
+        students.Add(new Student("Oliver", 18));
+        students.Add(new Student("Tania", 10));
+
+        students.Sort();
+
+        //foreach(Student stud in students)
+        //{
+            Debug.Log(message: $" L'élève le plus agé est {students[0].name} ");
+        //}
+        
+    }
+
+
+}
+
+

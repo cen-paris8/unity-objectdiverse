59413985f2ac496056cd1fa7b4d6dc8cc71eeb47
xofni <fakenewsProject@hotmail.com>
2019-09-16 12:15:38 +0200

    Add test

--- Assets/Scripts/Test/Test.cs
+++ Assets/Scripts/Test/Test.cs
@@ -20,10 +20,47 @@ public class Test : MonoBehaviour
         //{
             Debug.Log(message: $" L'élève le plus agé est {students[0].name} ");
         //}
-        
+
+        double[] ts = { 7, -10, 13, 8, 4, -7.2, -12, -3.7, 3.5, -9.6, 6.5, -1.7, -6.2, 7 };
+        double result = Solution.ClosestToZero(ts);
+
+        Debug.Log("Result: " + result);
     }
 
 
+}
+
+
+class Solution
+{
+
+    public static double ClosestToZero(double[] ts)
+    {
+        Array empty = new Array[0];
+
+        if (ts == empty)
+            return 0.0;
+
+        double lastTemp = 5526; // -273
+        int maxIndex = 0;
+        
+        for (int i = 0; i < ts.Length; i++)
+        {
+
+            if (ts[i] < 0 && Math.Abs(ts[i]) == lastTemp)
+            {
+                break;
+            }
+
+            if (Math.Abs(ts[i]) < lastTemp)
+            {
+                maxIndex = i;
+                lastTemp = Math.Abs(ts[i]);
+            }
+            
+        }
+        return ts[maxIndex];
+    }
 }
 
 
